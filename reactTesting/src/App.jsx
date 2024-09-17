3a469f82ff8ae6896b8afb54e33f687e9173046c
lekcik <cultcrib@gmail.com>
2024-09-17 19:51:42 +0200

    type error handling

--- reactTesting/src/App.jsx
+++ reactTesting/src/App.jsx
@@ -3,7 +3,8 @@ import Card from './components/Card'
 function App() {
     return (
       <>
-        <Card iconUrl='../src/assets/cardIconDev.png' name='Vladyslav' description='Web architect | Frontend developer | React master' />
+        <Card iconUrl='../src/assets/cardIconDev.png' name={false} description='Web architect | Frontend developer | React master' />
+        <Card iconUrl='../src/assets/cardIconDev.png' name='Ivan' description='No job' />
       </>
     );
 }
