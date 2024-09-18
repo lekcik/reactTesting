f161648ece8d18fdc687a95875b03cd27e56714f
lekcik <cultcrib@gmail.com>
2024-09-18 15:34:34 +0200

    cards styling added

--- reactTesting/src/components/Card.jsx
+++ reactTesting/src/components/Card.jsx
@@ -2,12 +2,16 @@ import '../css/Card.css'
 
 function Card(props) {
     return(
-        <section className="planCard">
+        <section className="planCard" aria-checked={props.checked}>
             <h2>{props.title}</h2>
             <h3>{props.price}$ per month</h3>
             <p>{props.description}</p>
         </section>
     );
+}
+
+Card.defaultProps = {
+    checked: ''
 }
 
 export default Card
--- reactTesting/src/components/Radio.jsx
+++ reactTesting/src/components/Radio.jsx
@@ -14,15 +14,15 @@ function Radio() {
             <section className='radioContainer'>
                 <input onChange={selectedPlanChangeHandler} checked={selectedPlan === 'free'} type="radio" id="planFree" name="choosePlan" value="free" />
                 <label for="planFree">
-                    <Card title="Free" price="0" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quibusdam quisquam asperiores ut laborum, voluptatem veniam reiciendis ducimus dignissimos, sequi eligendi voluptas ipsa."/>
+                    <Card checked={selectedPlan === 'free' ? 'checked' : ''} title="Free" price="0" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quibusdam quisquam asperiores ut laborum, voluptatem veniam reiciendis ducimus dignissimos, sequi eligendi voluptas ipsa."/>
                 </label>
                 <input onChange={selectedPlanChangeHandler} checked={selectedPlan === 'vip'} type="radio" id="planVIP" name="choosePlan" value="vip" />
                 <label for="planVIP">
-                    <Card title="VIP" price="10" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quibusdam quisquam asperiores ut laborum, voluptatem veniam reiciendis ducimus dignissimos, sequi eligendi voluptas ipsa."/>
+                    <Card checked={selectedPlan === 'vip' ? 'checked' : ''} title="VIP" price="10" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quibusdam quisquam asperiores ut laborum, voluptatem veniam reiciendis ducimus dignissimos, sequi eligendi voluptas ipsa."/>
                 </label>
                 <input onChange={selectedPlanChangeHandler} checked={selectedPlan === 'premium'} type="radio" id="planPremium" name="choosePlan" value="premium" />
                 <label for="planPremium">
-                    <Card title="Premium" price="25" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quibusdam quisquam asperiores ut laborum, voluptatem veniam reiciendis ducimus dignissimos, sequi eligendi voluptas ipsa."/>
+                    <Card checked={selectedPlan === 'premium' ? 'checked' : ''} title="Premium" price="25" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quibusdam quisquam asperiores ut laborum, voluptatem veniam reiciendis ducimus dignissimos, sequi eligendi voluptas ipsa."/>
                 </label>
             </section>
             <p>Selected plan: {selectedPlan}</p>
