0c10c423ea0309fed409796a3fd615677d8d0910
lekcik <cultcrib@gmail.com>
2024-09-21 13:07:08 +0200

    timer component created

--- reactTesting/src/components/Timer.jsx
+++ reactTesting/src/components/Timer.jsx
@@ -6,14 +6,16 @@ function Timer() {
     let interval = useRef();
 
     function start() {
-        if(!interval.current) {
+        if (!interval.current) {
+            prevTimeRef.current = Date.now();
+            
             interval.current = setInterval(() => {
                 const currTime = Date.now();
                 const elapsed = currTime - prevTimeRef.current;
     
                 setTimePassed((t) => t + elapsed);
                 prevTimeRef.current = currTime;
-            }, 10)
+            }, 10);
             console.log('interval added');
         }
     }
@@ -23,11 +25,33 @@ function Timer() {
         interval.current = null;
     }
 
+    function clear() {
+        stop();
+        setTimePassed(0);
+        prevTimeRef.current = 0;
+    }
+
+    function formatTime() {
+        const time = timePassed;
+
+        let seconds = time / 1000
+        let minutes = 0;
+
+        if (seconds > 60) {
+            minutes = Math.floor(seconds / 60);
+        }
+        seconds = Math.floor(seconds % 60);
+
+        let output = `${minutes}:${seconds}`;
+        
+        return output;
+    }
+
     return (
         <section className="timer">
-            <p>{timePassed / 1000}</p>
+            <p>{formatTime()}</p>
             <button onClick={start}>Start</button>
-            <button>Restart</button>
+            <button onClick={clear}>Clear</button>
             <button onClick={stop}>Stop</button>
         </section>
     );
